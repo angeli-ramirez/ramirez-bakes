170880c0ca314f79bcb98d46166c0bb64e9268de
angeli-ramirez <angelicramirez@su.edu.ph>
2024-01-24 17:03:20 +0800

    Create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './features/login/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginPageComponent },
+    { path: 'login', component: LoginPageComponent }
+];
