7a52ed58b5e6638129c76f1bfa58523fd37cb104
angeli-ramirez <angelicramirez@su.edu.ph>
2024-01-31 00:04:06 +0800

    Update admin navbar ui

--- src/app/components/admin-navbar/admin-navbar.component.ts
+++ src/app/components/admin-navbar/admin-navbar.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './admin-navbar.component.html',
   styleUrl: './admin-navbar.component.css'
 })
 export class AdminNavbarComponent {
   isActive(route: string): boolean {
+    console.log(route);
     return true;
   }
 }
